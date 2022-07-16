b839b6d8e9019d664880807025b6434dabbd9238
Mikhail <bulanjova@mail.ru>
2022-07-16 21:10:18 +0300

    add

--- src/test/java/cloud/autotests/config/ProjectConfig.java
+++ src/test/java/cloud/autotests/config/ProjectConfig.java
@@ -5,7 +5,6 @@ import org.aeonbits.owner.Config;
 @Config.LoadPolicy(Config.LoadType.MERGE)
 @Config.Sources({
         "system:properties",
-        "classpath:config/local.properties",
         "classpath:config/remote.properties"
 })
 public interface ProjectConfig extends Config {
---
+++ src/test/java/cloud/autotests/tests/DemowebshopTests.java
@@ -0,0 +1,71 @@
+package cloud.autotests.tests;
+
+
+import cloud.autotests.tests.TestBase;
+import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.WebDriverRunner;
+import com.codeborne.selenide.logevents.SelenideLogger;
+import io.qameta.allure.Story;
+import io.qameta.allure.selenide.AllureSelenide;
+import io.restassured.RestAssured;
+import org.junit.jupiter.api.*;
+import org.openqa.selenium.Cookie;
+
+import static cloud.autotests.helpers.AllureRestAssuredFilter.withCustomTemplates;
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selenide.*;
+import static io.qameta.allure.Allure.step;
+import static io.restassured.RestAssured.given;
+
+@Story("Login tests")
+public class DemowebshopTests {
+
+    static String login = "qaguru@qa.guru",
+            password = "qaguru@qa.guru1",
+            authCookieName = "NOPCOMMERCE.AUTH";
+
+    @BeforeAll
+    static void configure() {
+        SelenideLogger.addListener("AllureSelenide", new AllureSelenide());
+
+        Configuration.baseUrl = "http://demowebshop.tricentis.com";
+        RestAssured.baseURI = "http://demowebshop.tricentis.com";
+    }
+
+    @AfterEach
+    void afterEach() {
+        closeWebDriver();
+    }
+
+    @Test
+    @Tag("demowebshop")
+    @DisplayName("Successful authorization to some demowebshop (API + UI)")
+    void loginWithApiAndCustomListenerTest() {
+        step("Get cookie by api and set it to browser", () -> {
+            String authCookieValue = given()
+                    .filter(withCustomTemplates())
+                    .contentType("application/x-www-form-urlencoded")
+                    .formParam("Email", login)
+                    .formParam("Password", password)
+                    .log().all()
+                    .when()
+                    .post("/login")
+                    .then()
+                    .log().all()
+                    .statusCode(302)
+                    .extract().cookie(authCookieName);
+
+            step("Open minimal content, because cookie can be set when site is opened", () ->
+                    open("/Themes/DefaultClean/Content/images/logo.png"));
+            step("Set cookie to to browser", () -> {
+                Cookie authCookie = new Cookie(authCookieName, authCookieValue);
+                WebDriverRunner.getWebDriver().manage().addCookie(authCookie);
+            });
+        });
+
+        step("Open main page", () ->
+                open(""));
+        step("Verify successful authorization", () ->
+                $(".account").shouldHave(text(login)));
+    }
+}
